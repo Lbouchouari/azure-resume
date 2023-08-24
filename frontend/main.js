519adda7e3fba8668fd4572f2b618524fc4f010c
ahmed <lavishxa@gmail.com>
2023-08-24 12:04:20 +0100

    Deployed Storage and Function

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
+const functionApiURL = 'https://getresumecounter-lbc.azurewebsites.net/api/getresumecounter?code=0V9VN0NULmc5cnLMPwaiVxl97WcWJQpcqqNc4oPEpo1vAzFug1VRYQ%3D%3D'
 const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(localFunctionApi).then(response => {
+    fetch(functionApiURL).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
